3a84d54e104e3470df16ddb7ae9376fda7bab386
Leonard Storkenmaier <l-storkenmaier@gmx.de>
2026-09-06 22:55:07 +0200

    handling cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
+  const isDevelopment = process.env.NODE_ENV === 'development';
+
+
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  if (isDevelopment) {
+    app.enableCors({
+      origin: 'http://localhost:4200',
+      credentials: true,
+    });
+
+  }
 }
 await bootstrap();
